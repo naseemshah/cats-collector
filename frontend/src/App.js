783ee287daf88509ee74e88d3757feb5e923be62
Naseem Shah <naseemshahvarikodan@gmail.com>
2021-08-26 19:32:58 +0530

    add dash page route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,15 @@ import {
 } from "react-router-dom";
 
 import HomePage from './components/pages/Home';
+import Dashboard from './components/pages/Dashboard';
 
 function App() {
   return (
     <Router>
         <Switch>
+          <Route path="/dash">
+            <Dashboard />
+          </Route>
           <Route path="/">
             <HomePage />
           </Route>
---
+++ frontend/src/components/pages/Dashboard/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Dashboard(props) {
+    return (
+        <StyledDashboard>
+            Dashboard
+        </StyledDashboard>
+    );
+}
+
+export default Dashboard  ;
+
+let StyledDashboard = styled.div`
+
+
+`
