cc28e508a17d20c8f139c2e2820ecff1decd0272
Naseem Shah <naseemshahvarikodan@gmail.com>
2021-08-26 20:01:59 +0530

    add dash header

---
+++ frontend/src/components/pages/Dashboard/GridSection.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function GridSection(props) {
+    return (
+        <StyledGridSection>
+            grid section
+        </StyledGridSection>
+    );
+}
+
+export default GridSection;
+
+let StyledGridSection = styled.section`
+
+
+`
--- frontend/src/components/pages/Dashboard/index.js
+++ frontend/src/components/pages/Dashboard/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import GridSection from './GridSection';
 
 function Dashboard(props) {
     return (
         <StyledDashboard>
-            Dashboard
+            <section className="dash-header">
+                <h4>Cats Collector</h4>
+                <h1>Collector's Dashboard</h1>
+            </section>
+            <GridSection />
         </StyledDashboard>
     );
 }
@@ -12,6 +17,21 @@ function Dashboard(props) {
 export default Dashboard  ;
 
 let StyledDashboard = styled.div`
-
-
+    width: 80vw;
+    margin: 0px auto;
+    .dash-header{
+        color: #552400;
+        margin: 10px 0 0 0;
+        padding-bottom: 10px;
+        border-bottom: 2px solid black; 
+        /* background-color: aqua; */
+        h4{
+            padding: 0;
+            margin: 0;
+        }
+        h1{
+            padding: 0;
+            margin: 0;
+        }
+    }
 `
